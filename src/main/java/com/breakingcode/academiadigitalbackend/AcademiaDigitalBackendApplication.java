c146b433daa8323ab7ad72e3f13021c298f8b233
MauricioTrainingAssuresoft <mauricio.gamarra@training.assuresoft.com>
2022-05-12 09:52:13 -0400

    project created

---
+++ src/main/java/com/breakingcode/academiadigitalbackend/AcademiaDigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.breakingcode.academiadigitalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaDigitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaDigitalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breakingcode/academiadigitalbackend/AcademiaDigitalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakingcode.academiadigitalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDigitalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
