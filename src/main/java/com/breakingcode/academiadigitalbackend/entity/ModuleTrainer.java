d392c89ee67a9976d691fec32c0e26b0446e42a1
MauricioTrainingAssuresoft <mauricio.gamarra@training.assuresoft.com>
2022-05-12 20:50:44 -0400

    repository dummy methods created

--- src/main/java/com/breakingcode/academiadigitalbackend/entity/ModuleTrainer.java
+++ src/main/java/com/breakingcode/academiadigitalbackend/entity/ModuleTrainer.java
@@ -1,6 +1,12 @@
 package com.breakingcode.academiadigitalbackend.entity;
 
+import java.util.UUID;
+
 public class ModuleTrainer extends Person{
     private String feedback;
     private String module;
+
+    public ModuleTrainer(UUID guid, String firtsName, String lastName, String email, String address, String mobile) {
+        super(guid, firtsName, lastName, email, address, mobile);
+    }
 }
--- src/main/java/com/breakingcode/academiadigitalbackend/entity/Trainee.java
+++ src/main/java/com/breakingcode/academiadigitalbackend/entity/Trainee.java
@@ -1,10 +1,20 @@
 package com.breakingcode.academiadigitalbackend.entity;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Trainee extends Person{
     private List<Modules> scoreModule;
     private String headTrainer;
     private String group;
     private String feedback;
+
+    public Trainee(List<Modules> scoreModule, String headTrainer, String group, String feedback) {
+        super(UUID.fromString("24b2286e-330b-4fac-8cc0-9eeee5cbabce"), "Pedro", "Picapiedra",
+                "picapiedra@gmail.com", "piedra dura 12", "56254355");
+        this.scoreModule = scoreModule;
+        this.headTrainer = headTrainer;
+        this.group = group;
+        this.feedback = feedback;
+    }
 }
